4a44946d915ce7d1f22e134048de6a5407ac2e7d
Penguin-Run <penguinlgb@gmail.com>
2020-10-26 23:21:59 +0300

    added separate bilds for each work for main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "include/IO_manager.h"
 
+#define FILENAME_INPUT "/Users/Ivan/TPark-SEM1/C-HW2-TP/test_data/book.txt"
 
 int main() {
-
-    work_from_file("/Users/Ivan/TPark-SEM1/C-HW2-TP/test_data/book.txt", NULL); // NULL == stdout by default
+    
+    work_from_file(FILENAME_INPUT, NULL); // NULL == stdout by default
 
     return 0;
 }
