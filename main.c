a766fda3626594715eb0f29093c5495d58fce288
Penguin-Run <penguinlgb@gmail.com>
2020-10-28 22:37:06 +0300

    valgrind edits

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "include/IO_manager.h"
 
 #define FILENAME_INPUT "../test_data/book.txt"
+#define FILENAME_OUTPUT "../test_data/test_results/book.txt_results"
 
 // TODO: пофиксить утечки valgrind в тестах и в main
 
 int main() {
 
-    work_from_file(FILENAME_INPUT, NULL); // NULL == stdout by default
+    work_from_file(FILENAME_INPUT, FILENAME_OUTPUT); // NULL == stdout by default
 
     return 0;
 }
